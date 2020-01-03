83d8e41dc8f54bc669985857f9095348ba9d302c
AnwarHemdenee <anwar.hemdene@gmail.com>
2020-01-03 15:09:41 +0100

    added sign up

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import * as uuidv4 from 'uuid/v4';
+import { URL, USERS, USER} from '../../assets/variables';
+import { HttpClient } from  '@angular/common/http';
+import {AuthenticationService} from '../services/authentication.service';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService,
+  	public httpClient: HttpClient) { }
 
   ngOnInit() {
   }
-  onSubmit(event: NgForm) {
-  	console.log('event :: ', event.value.confirmPassword);
-  	console.log('event :: ', event.value.password);
-  	if (verifyUsernameExist(event.value.username)){
+  async onSubmit(event: NgForm) {
+   	if (true){
   		if (event.value.confirmPassword === event.value.password) {
-	  		console.log('okkk')
+	  		console.log('okkk');
+	  		await this.createUser(event.value.username, event.value.confirmPassword);
 	  	}else {
 	  		console.log('not ok');
 	  	}
@@ -26,5 +31,30 @@ export class SignUpPage implements OnInit {
   }
   verifyUsernameExist(username){
   	return true;
+  }
+    createUser(username, password) {
+    const id = uuidv4();
+    const createdAt = new Date();
+    let url = URL + USERS + USER + id;
+    this.httpClient.post(url, {
+    "id": id,
+    "username": username,
+    "password": password,
+    "createdAt": createdAt
+    })
+    .subscribe((res) => {
+      console.log('result here :: ', res);
+      if (res.result === "created") {
+      	this.loginUser();
+      }else {
+      	console.log('error');
+      }
+      // console.log('item id: ', res._id );
+      // console.log('result : ', res.result)
+    },
+      (error) => console.log(error));
+  }
+    loginUser() {
+    this.authenticationService.login();
   }
 }
--- src/assets/variables.ts
+++ src/assets/variables.ts
@@ -1,3 +1,6 @@
 export const URL = 'http://localhost:9200/';
 export const CATALOG = 'catalog/';
 export const QUOTES = 'quotes/';
+
+export const USERS = 'users/';
+export const USER = 'user/';
